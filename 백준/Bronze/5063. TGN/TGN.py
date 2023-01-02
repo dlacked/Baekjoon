41b255b3e32780c668e752ac5146ad99c5c66f22
임창용 <98292666+dlacked@users.noreply.github.com>
2023-01-02 10:19:19 +0900

    [Bronze III] Title: TGN, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,9 @@
+rep = int(input())
+for i in range(rep):
+    r, e, c = map(int, input().split())
+    if r > e - c:
+        print("do not advertise")
+    elif r == e - c:
+        print("does not matter")
+    else:
+        print("advertise")
